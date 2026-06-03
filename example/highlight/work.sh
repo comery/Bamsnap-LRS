a75a3009bf9c1ec1dffc7f47153a62f58825f55b
wanyi <1476347384@qq.com>
2026-06-03 11:50:37 +0800

    Update work.sh

--- example/highlight/work.sh
+++ example/highlight/work.sh
@@ -1,3 +1,3 @@
 python  bin/bamsnap-lrs highlight  --highlight-vcf region_chr21.vcf.gz \
   --bam sample1.hignlignt.bam --bam sample2.hignlignt.bam \
-  --pos chr21:20462000-20467000 --out highlight.svg #--fa chr21.fa
+  --pos chr21:20462000-20467000 --out highlight.svg --max-reads 50 #--fa chr21.fa
