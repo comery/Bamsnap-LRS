3fb3829df80e57f9bcc4857a4e0eb55625f76781
wanyi <1476347384@qq.com>
2026-06-04 17:50:45 +0800

    Update work.sh

--- example/SV_show/work.sh
+++ example/SV_show/work.sh
@@ -1,17 +1,43 @@
-## insertion
-python  /data/work/01.bamsnap_lrs/07.modify.v7/Bamsnap-LRS-main/bin/bamsnap-lrs dna --bam insertion.Cyclone.bam --bam insertion.HIFI.bam --bam insertion.ONT.bam --regions insertion.vcf  --out-prefix insertion.svg  --show-axis --show-coverage 
-python  /data/work/01.bamsnap_lrs/07.modify.v7/Bamsnap-LRS-main/bin/bamsnap-lrs dna --bam insertion.HIFI.bam --regions insertion.vcf  --out-prefix insertion.onebam.svg  --show-axis --show-coverage 
+#!/bin/bash
 
+# ====================== auto check FASTA file ======================
+FASTA_FILE="chm13v2.0.fasta"
+
+if [ -f "$FASTA_FILE" ]; then
+    echo "reference exist $FASTA_FILE, run command with --fa"
+    FASTA_ARG="--fa $FASTA_FILE"
+else
+    echo "no reference $FASTA_FILE, run command no --fa"
+    echo "reference can download from https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/analysis_set/chm13v2.0.fa.gz"
+    FASTA_ARG=""
+fi
+
+## insertion
+python  ../../bin/bamsnap-lrs dna \
+  --bam insertion.Cyclone.bam --bam insertion.HIFI.bam --bam insertion.ONT.bam \
+  --regions insertion.vcf  --out-prefix insertion.svg  --show-axis --show-coverage \
+  $FASTA_ARG
+python  ../../bin/bamsnap-lrs dna \
+  --bam insertion.HIFI.bam --regions insertion.vcf  --out-prefix insertion.onebam.svg  --show-axis --show-coverage \
+  $FASTA_ARG
 
 ## deletion
-python  /data/work/01.bamsnap_lrs/07.modify.v7/Bamsnap-LRS-main/bin/bamsnap-lrs dna --bam deletion.Cyclone.bam --bam deletion.HIFI.bam --bam deletion.ONT.bam --regions deletion.vcf  --out-prefix deletion.svg  --show-axis --show-coverage 
-python  /data/work/01.bamsnap_lrs/07.modify.v7/Bamsnap-LRS-main/bin/bamsnap-lrs dna --bam deletion.HIFI.bam --regions deletion.vcf  --out-prefix deletion.onebam.svg  --show-axis --show-coverage 
-
+python  ../../bin/bamsnap-lrs dna \
+  --bam deletion.Cyclone.bam --bam deletion.HIFI.bam --bam deletion.ONT.bam \
+  --regions deletion.vcf  --out-prefix deletion.svg  --show-axis --show-coverage \
+  $FASTA_ARG
+  
+python  ../../bin/bamsnap-lrs dna \
+  --bam deletion.HIFI.bam --regions deletion.vcf  --out-prefix deletion.onebam.svg  --show-axis --show-coverage \
+  $FASTA_ARG
 
 ## duplication
-python  /data/work/01.bamsnap_lrs/07.modify.v7/Bamsnap-LRS-main/bin/bamsnap-lrs dna --bam duplication.HIFI.bam --regions duplication.vcf --out-prefix duplication.svg --show-axis --show-coverage --show-supp
-
+python  ../../bin/bamsnap-lrs dna \
+  --bam duplication.HIFI.bam --regions duplication.vcf --out-prefix duplication.svg --show-axis --show-coverage --show-supp \
+  $FASTA_ARG
 
 ## inversion
-python  /data/work/01.bamsnap_lrs/07.modify.v7/Bamsnap-LRS-main/bin/bamsnap-lrs dna --bam inversion.HIFI.bam --regions inversion.vcf --out-prefix inversion.svg --show-axis --show-coverage --show-supp
+python  ../../bin/bamsnap-lrs dna \
+  --bam inversion.HIFI.bam --regions inversion.vcf --out-prefix inversion.svg --show-axis --show-coverage --show-supp \
+  $FASTA_ARG
 
